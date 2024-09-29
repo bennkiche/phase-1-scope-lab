25bd2e509f248cd91cf417f651af94cec4c1d5e8
Benn <bennkiche7@gmail.com>
2024-09-29 16:40:54 +0300

    updatetask

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Declare bestCustomer in global scope
+  }
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; // Change the global bestCustomer
+  }
+const leastFavoriteCustomer = 'some value'; // Assign an initial value
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another value'; // This will throw an error
+  }
+  
